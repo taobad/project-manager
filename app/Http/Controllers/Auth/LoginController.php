ac8df02e4530755baec9d1fe971b0b5e23b6d610
badmustaofeeq <badmustaofeeq@gmail.com>
2021-09-01 01:27:25 +0100

    Redirect Portal

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,9 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         $ums_url = getenv('UMS_URL');
-        return redirect()->away($ums_url . 'login');
+        $redirect_portal = Crypt::encryptString('pm');
+
+        return redirect()->away($ums_url . 'login?redirect_portal = ' . $redirect_portal);
 
         //Old code below;
         //return view('auth.login');
