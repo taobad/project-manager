e93b54514d124c231f9c194803db227a2abc5ff8
Mubarak Moshood <moshood_mubarak90@yahoo.com>
2021-11-02 00:23:54 +0100

    fix error

--- Modules/Calendar/Resources/views/modal/event.blade.php
+++ Modules/Calendar/Resources/views/modal/event.blade.php
@@ -427,8 +427,9 @@
                 @if ($event->project_id > 0)
                 <li class="list-group-item">
                     <div class="pull-right">
-                        <a class="font-semibold text-indigo-600" href="{{ route('projects.view', $event->project_id) }}">
-                            {{ $event->eventable->name }}
+                        <a class="font-semibold text-indigo-600" href="{{ route('projects.view', $event->project_id) }}">   
+                            {{--To do: find out how to get project name--}}
+                            {{ 'fix: Supposed project name' }}
                         </a>
                     </div>
                     @langapp('project')
